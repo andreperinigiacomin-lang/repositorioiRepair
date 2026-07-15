72ff96d129c6c036e47c13f0584099be48d438b5
André Perini <andreperinigiacomin@gmail.com>
2026-07-15 14:46:23 -0300

    feat: faz a navegacao por paginas funcionar(se digitar /, /clients, etc na url

--- ijunior-task-board/src/App.tsx
+++ ijunior-task-board/src/App.tsx
@@ -1,60 +1,44 @@
-import Header from "./components/Header";
-import ServiceCard from "./components/ServiceCard";
-import NewServiceForm from "./components/NewServiceForm";
-import type { ServiceOrder } from "./types/ServiceOrder";
-import StatusSummary from "./components/StatusSummary";
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-export function App() {
-  const [orders, setOrders] = useState<ServiceOrder[]>([]);
-  const addOrder = (order: {
-    cliente: string;
-    aparelho: string;
-    defeito: string;
-    status: string;
-  }) => {
-    const newOrder: ServiceOrder = {
-      id: Date.now(),
-      data: new Date().toLocaleDateString("pt-BR"),
-      cliente: order.cliente,
-      aparelho: order.aparelho,
-      defeito: order.defeito,
-      status: order.status,
-    };
-    setOrders([...orders, newOrder]);
-  };
-  const removeOrder = (id: number) => {
-    setOrders(orders.filter((order) => order.id !== id));
-  };
-  return (
-    <>
-      <div>{/*bloco principal*/}
-        <Header />
-        <main className="mt-8 ml-8">
-          <div className="flex gap-8">{/*espacamento dos blocos*/}
+import MainLayout from "./layouts/MainLayout";
 
-            <div className="w-1/3 flex flex-col gap-4 items-stretch">{/*segura 1/3 da tela para adicionar nova secao*/}
+import DashboardPage from "./pages/DashboardPage";
+import ClientsPage from "./pages/ClientsPage";
+import ServiceOrdersPage from "./pages/ServiceOrdersPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
-              <NewServiceForm onAddOrder={addOrder} />
+export const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
 
-              <div>{/*bloco de status summary*/}
-                <StatusSummary orders={orders} />
-              </div>{/*bloco de status summary*/}
-            </div>{/*segura 1/3 da tela para o adicionar nova secao e status summary*/}
+                <Route element={<MainLayout />}>
 
-            <div className="w-2/3 grid grid-cols-3 gap-x-4 gap-y-2">{/*segura o bloco das cards 2/3 da tela*/}
+                    <Route
+                        path="/"
+                        element={<DashboardPage />}
+                    />
 
-              {orders.map((order) => (
-                <ServiceCard
-                  key={order.id}
-                  order={order}
-                  onRemoveOrder={removeOrder}
+                    <Route
+                        path="/clients"
+                        element={<ClientsPage />}
+                    />
+
+                    <Route
+                        path="/service-orders"
+                        element={<ServiceOrdersPage />}
+                    />
+
+                </Route>
+
+                <Route
+                    path="*"
+                    element={<NotFoundPage />}
                 />
-              ))}
-            </div>{/*segura o bloco das cards 2/3 da tela*/}
-          </div>{/*espacamento dos blocos*/}
-        </main>
-      </div>{/*bloco principal*/}
-    </>
-  );
-}
+
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default App;
