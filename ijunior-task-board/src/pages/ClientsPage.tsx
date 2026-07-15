4a7f928e942a4f97ca2e62ab6872d8553c33ecbf
André Perini <andreperinigiacomin@gmail.com>
2026-07-15 20:24:43 -0300

    feat: adiciona a logica do useEffect quando a pagina troca para a secao 'clients'. Enquanto isso, aparece um loading na tela

--- ijunior-task-board/src/pages/ClientsPage.tsx
+++ ijunior-task-board/src/pages/ClientsPage.tsx
@@ -1,11 +1,45 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import { getAllClients } from "../services/clientService";
+import type { Client } from "../types";
 
 const ClientsPage = () => {
-  return (
-    <div>
-      clientPage
-    </div>
-  )
-}
+    const [clients, setClients] = useState<Client[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-export default ClientsPage
+    useEffect(() => {
+        async function loadClients() {
+            try {
+                const data = await getAllClients();
+                console.log(data);
+                setClients(data);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        loadClients();
+    }, []);
+
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <div>
+            <h1 className="text-2xl font-bold mb-6">Clients</h1>
+
+            <ul>
+                {clients.map((client) => (
+                    <li key={client.id}>
+                        {client.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ClientsPage;
