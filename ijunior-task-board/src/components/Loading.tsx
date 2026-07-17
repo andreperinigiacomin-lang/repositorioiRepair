53af3607968d791669fa42bccbca817ab207b2fb
André Perini <andreperinigiacomin@gmail.com>
2026-07-17 10:31:11 -0300

    style: adiciona um spinner para a tela de loading

--- ijunior-task-board/src/components/Loading.tsx
+++ ijunior-task-board/src/components/Loading.tsx
@@ -1,6 +1,7 @@
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center py-12">
+    <div className="flex justify-center items-center py-12 gap-4">
+        <div className="w-10 h-10 border-4 border-slate-200 border-t-[#10b981] rounded-full animate-spin"></div>
       <p className="text-slate-500 text-lg font-medium">
         Carregando...
       </p>
