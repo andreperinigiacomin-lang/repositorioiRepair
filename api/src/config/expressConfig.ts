1dba738d0a37e5f029e78ef26ccc7267027b01ff
André Perini <andreperinigiacomin@gmail.com>
2026-08-04 10:46:32 -0300

    feat: adiciona mais mudancas estruturais

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,7 +1,9 @@
+import cookieParser from 'cookie-parser'
 import express from "express";
 import taskRoutes from "../routes/taskRoutes";
 
 const app = express();
+app.use(cookieParser())
 app.use(express.json());
 app.use(taskRoutes);
 
