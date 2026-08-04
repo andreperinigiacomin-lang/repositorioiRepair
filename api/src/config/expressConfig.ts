c0146e459f5bf8d0a700b4241a14fdeab4464666
André Perini <andreperinigiacomin@gmail.com>
2026-08-04 11:44:59 -0300

    fix: refazendo e reentendendo o auth routes e o expressConfig

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,10 +1,12 @@
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
 import express from "express";
 import taskRoutes from "../routes/taskRoutes";
+import { authRoutes } from '../domains/auth/auth.routes';
 
 const app = express();
-app.use(cookieParser())
 app.use(express.json());
-app.use(taskRoutes);
+app.use(cookieParser());
+app.use('/auth', authRoutes);
+app.use('/task', taskRoutes);
 
 export {app};
