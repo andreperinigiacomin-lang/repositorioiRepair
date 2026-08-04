60f9b66715c2a1d5f6818081d6558bf7fbce0647
André Perini <andreperinigiacomin@gmail.com>
2026-08-04 14:34:42 -0300

    feat: corrige o erro de inicializacao do servidor e adiciona o middleware de erroHandler

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -2,11 +2,14 @@ import cookieParser from 'cookie-parser';
 import express from "express";
 import taskRoutes from "../routes/taskRoutes";
 import { authRoutes } from '../domains/auth/auth.routes';
+import { errorHandler } from '../middlewares/errorHandler';
 
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/auth', authRoutes);
 app.use('/task', taskRoutes);
+app.use(errorHandler);
 
 export {app};
