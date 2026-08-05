7d54dd738774dcf097a3745d50a7a46fa50b10ad
André Perini <andreperinigiacomin@gmail.com>
2026-08-05 11:10:19 -0300

    feat: adiciona o metodo create no controller do clients

---
+++ api/src/domains/clients/cliente.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { ClientService } from "./client.service";
+
+const clientService = new ClientService();
+
+export class ClientController {
+
+    async create(req: Request, res: Response){
+        const {name, email, phone} = req.body;
+        const userId = req.user!.id;
+        const client =  await clientService.createClient(
+            name,
+            email,
+            phone,
+            userId,
+        );
+        return res.status(201).json(client);
+    }
+
+}
