6b1fda18c36ae2c63568024007a316a5fe6f57ef
André Perini <andreperinigiacomin@gmail.com>
2026-08-07 10:38:16 -0300

    feat: adiciona o private route e conecta ele no app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,51 @@
 import { BrowserRouter, Routes, Route } from "react-router";
-
 import MainLayout from "./layouts/MainLayout";
-
 import DashboardPage from "./pages/DashboardPage";
 import ClientsPage from "./pages/ClientsPage";
 import ServiceOrdersPage from "./pages/ServiceOrdersPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import { AuthProvider } from "./contexts/AuthContext";
+import { PrivateRoute } from "./routes/PrivateRoute";
+import { Login } from "./pages/Login";
 
 export const App = () => {
     return (
         <BrowserRouter>
-            <Routes>
-
-                <Route element={<MainLayout />}>
-
+            <AuthProvider>
+                <Routes>
                     <Route
-                        path="/"
-                        element={<DashboardPage />}
+                    path="/login"
+                    element={<login/>}
                     />
 
-                    <Route
-                        path="/clients"
-                        element={<ClientsPage />}
-                    />
+                    <Route element={<PrivateRoute/>}>
+                    <Route element={<MainLayout />}>
+                        <Route
+                            path="/"
+                            element={<DashboardPage />}
+                        />
 
-                    <Route
-                        path="/service-orders"
-                        element={<ServiceOrdersPage />}
-                    />
+                        <Route
+                            path="/clients"
+                            element={<ClientsPage />}
+                        />
 
+                        <Route
+                            path="/service-orders"
+                            element={<ServiceOrdersPage />}
+                        />
+
+                    </Route>
                 </Route>
 
-                <Route
-                    path="*"
-                    element={<NotFoundPage />}
-                />
 
-            </Routes>
+                    <Route
+                        path="*"
+                        element={<NotFoundPage />}
+                    />
+
+                </Routes>
+            </AuthProvider>
         </BrowserRouter>
     );
 };
---
+++ client/src/routes/PrivateRoute.tsx
@@ -0,0 +1,18 @@
+import { Navigate, Outlet } from "react-router";
+import { useAuth } from "../contexts/AuthContext";
+
+export function PrivateRoute(){
+    const { 
+        isAuthenticated, 
+        isLoading,} = useAuth();
+        
+        if(isLoading){
+            return <div>Carregando...</div>
+        }
+
+        if(!isAuthenticated){
+            return <Navigate to="/login" replace />;
+        }
+
+        return <Outlet />;
+}
