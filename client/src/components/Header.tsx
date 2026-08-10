54c53c6c17dab7cfbc1925dedc5f64d5f3a4eafa
André Perini <andreperinigiacomin@gmail.com>
2026-08-10 11:12:59 -0300

    feat: adiciona o botao de sair no header, ja funcional.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,17 @@
+import { useNavigate } from "react-router";
+import { useAuth } from "../contexts/AuthContext";
+
 const Header = () => {
+
+  const {logout} = useAuth();
+  const navigate = useNavigate();
+  async function handleLogout() {
+    await logout();
+    navigate("/login");
+  }
+
   return (
-    <header className=" bg-[#0f172a] text-white px-6 py-3 flex items-center justify-start border-b border-slate-800">
+    <header className=" bg-[#0f172a] text-white px-6 py-3 flex items-center justify-between border-b border-slate-800 ">
       <div className="flex items-center gap-4">{/*lado esquerdo*/}
         <div className="flex items-center gap-2 bg-[#10b981] px-3 py-1.5 rounded-lg border-[#10b981]/40">{/*logo e nome*/}
           <i className="bi bi-tools text-[#0f172a] text-2xl [-webkit-text-stroke:1px]"></i>
@@ -11,11 +22,19 @@ const Header = () => {
             Repair
           </span>
         </h1>
-      </div>{/*lado esquerdo*/}
       <div className="h-6 w-[1.5px] bg-slate-700 hidden sm:block ml-[25px]"></div>{/*linha divisoria */}
       <h3 className="text-slate-200 text-sm font-medium hidden sm:block ml-[15px]">
         Sistema de Ordens de Serviço
       </h3>
+      </div>{/*lado esquerdo*/}
+
+      <div>{/* lado direito */}
+      <button 
+      onClick={handleLogout}
+      className="px-4 py-2 text-sm font-semibold text-slate-300 rounded-lg hover:bg-emerald-300 text-white-700 transition">
+        Sair
+      </button>
+      </div>{/* lado direito */}
     </header>
   );
 };
