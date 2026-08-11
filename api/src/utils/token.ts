f34406143997c8287b4b35cd65de79e15a29773c
André Perini <andreperinigiacomin@gmail.com>
2026-08-11 11:02:57 -0300

    feat: diferencia os tipos de token

--- api/src/utils/token.ts
+++ api/src/utils/token.ts
@@ -1,20 +1,26 @@
 import  jwt  from "jsonwebtoken";
+import { access } from "node:fs";
 
 interface TokenPayLoad {
     id: number;
     email: string;
+    type: "access" | "refresh";
 }
-export function generateAccessToken(payload: TokenPayLoad): string{
-    return jwt.sign(
+export function generateAccessToken(payload: Omit<TokenPayLoad, "type">): string{
+    return jwt.sign({
         payload,
+        type: "access"
+    },
         process.env.JWT_SECRET!,{
             expiresIn: process.env.JWT_EXPIRES_IN as jwt.SignOptions['expiresIn'],
         }
     )
 }
-export function generateRefreshToken(payload: object){
-    return jwt.sign(
+export function generateRefreshToken(payload: Omit<TokenPayLoad, "type">): string{
+    return jwt.sign({
         payload,
+        type: "refresh",
+        },
         process.env.JWT_SECRET!,{
             expiresIn: process.env.JWT_REFRESH_EXPIRES_IN as jwt.SignOptions['expiresIn'],
         }
@@ -22,6 +28,9 @@ export function generateRefreshToken(payload: object){
 }
 
 interface VerifyTokenPayLoad extends TokenPayLoad{
+    id: number;
+    email: string;
+    type: "access" | "refresh";
     iat: number,
     exp: number,
 }
